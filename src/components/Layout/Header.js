f217e49513c79f8c754a8eb855726cadf8fbac59
kurashiva11 <kurashiva11@gmail.com>
2021-05-03 11:13:51 +0530

    added and styled header

---
+++ src/components/Layout/Header.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+import mealsImage from "../../assets/meals.jpg";
+import classes from "./Header.module.css";
+
+import HeaderCardButton from "./HeaderCartButton";
+
+
+function Header() {
+  return (
+    <>
+      <header className={classes.header}>
+      <h1>ReactMeals</h1>
+      <HeaderCardButton>Cart</HeaderCardButton>
+      </header>
+      <div className={classes['main-image']}>
+        <img src={mealsImage} alt="Meals JPG i missing or unable to Load"/>
+      </div>
+    </>
+  );
+}
+
+export default Header;
